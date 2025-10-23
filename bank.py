1e50b375491c0adcf7425342bf759c5036833184
vaishnavi-honnimani <01fe24bca154@kletech.ac.in>
2025-10-23 03:02:26 -0700

    save

--- bank.py
+++ bank.py
@@ -6,3 +6,9 @@ new_balance=initial_balance+deposit
 print(f"Initial Balance:",initial_balance)
 print(f"Deposit:",deposit)
 print(f"New Balance",new_balance)
+
+withdraw=float(input("Enter withdrawal amount"))
+new_balance-=withdraw
+
+print(f"withdraw:",withdraw)
+print(f"Final balance:",new_balance)
